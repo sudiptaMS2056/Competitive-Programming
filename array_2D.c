db2384fcd7b52977a47a1f83712e5fe78d459603
Sudipta Hazra <sudiptahazra512@gmail.com>
2026-09-17 10:19:44 +0530

    Clean up comments in array_2D.c
    Removed comments for input prompts in the binary search program.

--- array_2D.c
+++ array_2D.c
@@ -3,19 +3,15 @@ int main()
 {
     int arr[100], n, key;
     int low, high, mid, found = 0;
-    // Input array size
     printf("Enter the number of elements: ");
     scanf("%d", &n);
-    // Input sorted array
     printf("Enter %d elements in sorted order:\n", n);
     for (int i = 0; i < n; i++)
     {
         scanf("%d", &arr[i]);
     }
-    // Input element to search
     printf("Enter the element to search: ");
     scanf("%d", &key);
-    // Binary Search
     low = 0;
     high = n - 1;
     while (low <= high)
